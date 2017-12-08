944785cb20458da89b61af75852b2080c2f486ac
Matthew Sperry <mattjsperry@gmail.com>
2017-12-08 10:28:22 -0800

    Update package.json

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ app.use(express.static(__dirname + '/'))
    .use(cookieParser());
 
 let token;
+let port = 8888;
+
+if (process.env.PORT) {
+  port = process.env.PORT;
+}
 
 spotifyAuth((err, resp, body) => {
   token = body.access_token;
-  console.log('Listening on 8888');
-  app.listen(8888);
+  console.log(`Listening on ${port}`);
+  app.listen(port);
 });
 
 app.get('/token', (req, res) => {
